3767634131c320055259f75c26b3c8413e61da3b
Miket4528 <128186986+Miket4528@users.noreply.github.com>
2023-03-28 14:16:19 -0400

    moodtracker updates

---
+++ Mood.js
@@ -0,0 +1,52 @@
+class MoodEntry{
+
+    constructor(mood,date){
+        this.mood = mood;
+        this.date = date;
+
+    } 
+
+    /*consider moving switch to constructor/ swapping mplementation
+     (depends on how we want to interact w UI) */
+    get mood(){
+        switch(this.mood){
+            case 0: return "Image/color for null here";
+            case 1: return "Image/color for :(( here";
+            case 2: return "Image/color for :( here";
+            case 3: return "Image/color for :| here";
+            case 4: return "Image/color for :) here";
+            case 5: return "Image/color for :)) here";
+        }
+
+    }
+
+}
+
+class MoodTracker{
+    database = [];  //set of entries
+    checkedIn = false;
+
+    /**somehow runs on startup? */
+    checkIn(){
+        if(!checkedIn){
+            /** UI interaction to get info from user */
+            mood;
+            date;
+            this.database.push(new MoodEntry(mood, date));
+        }
+    }
+
+    /**reset/run at midnight? */
+    reset(){
+        if(checkedIn){
+            this.checkedIn = false;
+        }
+        else {
+            previousdate; // remove later once defined
+            this.database.push(new MoodEntry(0,previousdate))
+        }
+    }
+
+
+
+}
