bb33739ae82fa742894c54a6a4b4dbf3d6f7a235
Miket4528 <128186986+Miket4528@users.noreply.github.com>
2023-04-23 15:00:13 -0400

    help added

--- Mood.js
+++ Mood.js
@@ -1,29 +1,3 @@
-class MoodEntry{
-    mood = 0;
-    date;
-
-    constructor(m){
-        this.mood = m;
-        this.date = new Date();
-
-    } 
-
-    /*consider moving switch to constructor/ swapping mplementation
-     (depends on how we want to interact w UI) */
-    // get mood(){
-    //     switch(this.mood){
-    //         default: return "images/blue icon.png";
-    //         case 1: return "images/blue icon.png";
-    //         case 2: return "images/light blue icon.png";
-    //         case 3: return "images/yellow icon.png";
-    //         case 4: return "images/light green icon.png";
-    //         case 5: return "images/green icon.png";
-    //     }
-
-    // }
-    
-
-}
 
 class MoodTracker{
     database = localStorage.getItem("entries");  //set of entries
@@ -31,15 +5,14 @@ class MoodTracker{
 
 
     checkin(mood){
-        // var m = new MoodEntry(mood)
-        // this.database.push(m);
+
         localStorage.setItem("checkin", "true");
         var entries = localStorage.getItem("entries");
         if(entries!=null) localStorage.setItem("entries", entries + "," + mood)
         else localStorage.setItem("entries", mood)
         this.database = localStorage.getItem("entries").split(',');
     }
-    checkedIn(){
+    checkedIn(){ //not used
         if(localStorage.getItem("checkin") == "true") return true;
         else return false;
     }
